6237adf21946ee00056bdc51079ec0d37179504e
yakimka <ss.yakim@gmail.com>
2023-12-02 15:55:58 +0200

    Day 2 Part 2 (ez)

---
+++ day02/part2.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+import sys
+import timeit
+from pathlib import Path
+
+import pytest
+
+INPUT_TXT = Path(__file__).parent / "input.txt"
+
+
+def compute(s: str) -> int:
+    result = 0
+    for game_id, line in enumerate(s.splitlines(), start=1):
+        _, game_data = line.split(": ")
+        turns = game_data.split("; ")
+        max_red = -1
+        max_green = -1
+        max_blue = -1
+        for turn in turns:
+            for turn_data in turn.split(", "):
+                count, color = turn_data.split(" ")
+                count = int(count)
+                if color == "red" and count > max_red:
+                    max_red = count
+                elif color == "green" and count > max_green:
+                    max_green = count
+                elif color == "blue" and count > max_blue:
+                    max_blue = count
+
+        result += max_red * max_green * max_blue
+    return result
+
+
+INPUT_S = """\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+EXPECTED = 2286
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test_debug(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def test_input() -> None:
+    with open(INPUT_TXT) as f:
+        result = compute(f.read())
+
+    assert result == 71535
+
+
+def read_input() -> str:
+    with open(INPUT_TXT) as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    input_data = read_input()
+    print("Answer is:", compute(input_data))
+
+    if "-b" in sys.argv:
+        number_of_runs = 1000
+        bench_time = timeit.timeit(
+            "compute(data)",
+            setup="from __main__ import compute",
+            globals={"data": input_data},
+            number=number_of_runs,
+        )
+        print(f"{number_of_runs} runs took {bench_time} seconds")
