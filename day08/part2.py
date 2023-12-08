c5aa68c38d05997ef12c4f1d859f13780bd8c878
yakimka <ss.yakim@gmail.com>
2023-12-08 19:07:13 +0200

    Refactored Day 8 Part 2

--- day08/part2.py
+++ day08/part2.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+import math
 import sys
 import timeit
 from functools import reduce
-from itertools import count
 from pathlib import Path
 
 import pytest
@@ -13,58 +13,30 @@ import support as sup
 INPUT_TXT = Path(__file__).parent / "input.txt"
 
 
-def steps_generator(steps: str) -> str:
-    for i in count():
-        for step in steps:
-            yield i, step
-
-
 def compute(s: str) -> int:
     lines = s.splitlines()
     mapping = {}
     starting_nodes = []
     for line in lines[2:]:
-        line = line.replace(" = ", " ").replace("(", "").replace(")", "").replace(",", " ")
+        line = (
+            line.replace(" = ", " ").replace("(", "").replace(")", "").replace(",", " ")
+        )
         node_name, *values = line.split()
         mapping[node_name] = values
         if node_name.endswith("A"):
             starting_nodes.append(node_name)
 
-    current_list = list(starting_nodes)
-    steps_count = 0
+    counts = []
     steps_index = ["L", "R"]
-    starting_paths_count = len(starting_nodes)
-    steps_len = len(lines[0])
-    found_z = []
-    for i, (loop_num, step) in enumerate(steps_generator(lines[0])):
-        step_i = i % steps_len
-        steps_count += 1
-        for i, current in enumerate(current_list):
-            if current in mapping:
-                current_list[i] = mapping[current][steps_index.index(step)]
+    for node in starting_nodes:
+        count = 0
+        while not node.endswith("Z"):
+            map_i = count % len(lines[0])
+            node = mapping[node][steps_index.index(lines[0][map_i])]
+            count += 1
+        counts.append(count)
 
-            if current_list[i].endswith("Z"):
-                found_z.append((loop_num + 1, step_i + 1, i))
-
-        if len(found_z) > starting_paths_count and found_z[-1][2] == len(current_list) - 1:
-            break
-
-    data = {}
-    for item in found_z:
-        data.setdefault(item[2], []).append(item[0])
-    data = sorted([v[-1] - v[-2] for k, v in data.items()])
-    number = reduce(lowest_common_multiple, data)
-    return number * found_z[0][1]
-
-
-def greatest_common_divisor(a: int, b: int) -> int:
-    while b:
-        a, b = b, a % b
-    return a
-
-
-def lowest_common_multiple(a: int, b: int) -> int:
-    return a * b // greatest_common_divisor(a, b)
+    return reduce(math.lcm, counts)
 
 
 INPUT_S1 = """\
@@ -82,11 +54,14 @@ XXX = (XXX, XXX)
 EXPECTED1 = 6
 
 
-# @pytest.mark.parametrize("input_s,expected", [
-#     (INPUT_S1, EXPECTED1),
-# ])
-# def test_debug(input_s: str, expected: int) -> None:
-#     assert compute(input_s) == expected
+@pytest.mark.parametrize(
+    "input_s,expected",
+    [
+        (INPUT_S1, EXPECTED1),
+    ],
+)
+def test_debug(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
 
 
 def test_input() -> None:
